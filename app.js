d5f54e01bbd2904878d9fec22097a83356936a10
AbdureDev <abduahmannwr@gmail.com>
2023-06-17 09:48:32 +0300

    get method

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    console.log('Hello World!');
+});
+
+app.listen(3000, () => console.log('Listining on port 3000....'));
