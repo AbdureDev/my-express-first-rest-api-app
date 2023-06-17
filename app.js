75dacbb014d1c6636c92aaaa6512031115348002
AbdureDev <abduahmannwr@gmail.com>
2023-06-17 12:13:56 +0300

    response with id

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'JavaScript'},
+    {id: 2, name: 'C++'},
+    {id: 3, name: 'PHP'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    console.log('Hello World!');
+    console.log('Hello World!!!');
 });
 
-app.listen(3000, () => console.log('Listining on port 3000....'));
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not found');
+    res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listining on port ${port}....`));
